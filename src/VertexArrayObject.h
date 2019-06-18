1c8dfd6362bd30b02f3f11a3b2b1a463a4797700
lukas leitner <lukasleitner17@gmail.com>
2019-06-18 16:59:16 +0200

    Added VertexArrayObject class

---
+++ src/VertexArrayObject.h
@@ -0,0 +1,25 @@
+#ifndef VERTEX_ARRAY_OBJECT_H
+#define VERTEX_ARRAY_OBJECT_H
+
+#include <glad/glad.h>
+#include "Shader.h"
+
+class VertexArrayObject {
+private:
+    unsigned int m_VAO, m_VBO, m_EBO = 0;
+    Shader m_ShaderProgram;
+    unsigned int vertices_count;
+public:
+    VertexArrayObject();
+    //VertexArrayObject(float vertices[]);
+    //VertexArrayObject(float vertices[], unsigned int indices[]);
+    ~VertexArrayObject();
+
+    void setVertexData(float vertices[], int size);
+    void setVertexAttribPointer(int index, int size, int offset, int stride, bool normalized = false);
+    void setIndices(unsigned int indices[], int size);
+    void setShaderProgram(Shader shaderProgram);
+    void draw();
+};
+
+#endif
