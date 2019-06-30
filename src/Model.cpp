c6b10dbf9ca6ae1da2213be2fe85b88e96205d09
Lukas Leitner <lukasleitner17@gmail.com>
2019-06-30 11:28:57 +0200

    added Model class

---
+++ src/Model.cpp
@@ -0,0 +1,25 @@
+#include "Model.h"
+
+Model::Model(char *path){
+
+}
+
+void Model::Draw(Shader shader){
+
+}
+
+void Model::loadModel(std::string path){
+
+}
+
+void Model::processNode(aiNode *node, const aiScene *scene){
+
+}
+
+Mesh Model::processMesh(aiNode *node, const aiScene *scene){
+
+}
+
+std::vector<Texture> Model::loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName){
+    
+}
